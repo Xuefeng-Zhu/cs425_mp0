a1bca4b064080165e6c8819189d281e12233ff93
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-01-22 14:37:22 -0600

    send file through socket

--- client.py
+++ client.py
@@ -5,5 +5,10 @@ host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 
 s.connect((host, port))
-print s.recv(1024)
-s.close                     # Close the socket when done
+f = open('receive.pdf', 'w')
+temp = s.recv(1024)
+while temp:
+	f.write(temp)
+	temp = s.recv(1024)
+f.close()
+s.close()                     # Close the socket when done
--- server.py
+++ server.py
@@ -4,10 +4,11 @@ s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
-
 s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print 'Got connection from', addr
-   c.send('Thank you for connecting')
+   f = open('send.pdf')
+   c.sendall(f.read())
+   print "file sent"
    c.close()                # Close the connection
